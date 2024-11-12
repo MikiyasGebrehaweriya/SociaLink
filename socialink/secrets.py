393ef54d3db4b2c1ab033d1ace16b82328b95081
MikiyasGebrehaweriya <mikiyas.gebrehaweriya@code.berlin>
2024-11-12 05:00:36 +0100

    Secreats File Pushed

---
+++ socialink/secrets.py
@@ -0,0 +1,14 @@
+database_name = "SociaLink"
+database_password = "database_password"
+database_user = "postgres"
+database_host = "localhost"
+database_port = "5432"
+
+
+INSTAGRAM_CLIENT_ID = "3808893242668080"
+INSTAGRAM_CLIENT_SECRET = "7aaac885fdc5a22d9437b46ad645cb80"
+
+FACEBOOK_CLIENT_ID = "955643172746923"
+FACEBOOK_CLIENT_SECRET = "d5f91b0e66eb31cb810dec8fb0d968ca"
+
+
